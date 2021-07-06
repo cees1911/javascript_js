f400927e2f5128be4495c17c499a7a5a99bb287c
Cees Klap <ceesklap@gmail.com>
2021-07-06 12:18:59 +0200

    eerste commit

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd599a2ad-3543-429e-9360-5095a3c9b8f3';
+export const MODAL_CLOSE_SEC = 2.5;
